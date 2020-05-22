06983fea68872f299aeb4b2726e57c2f8430983c
Daniel Gonzalez Placito <daargonzalezpl@ittepic.edu.mx>
2020-05-22 17:30:02 -0600

    Exportar Excel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+import { ExporterService } from './services/exporter.service';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DataTableComponent } from './components/data-table/data-table.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTableModule } from '@angular/material/table';
+import { MatNativeDateModule } from '@angular/material/core';
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, DataTableComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatInputModule,
+    MatNativeDateModule,
+    MatTableModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ExporterService],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
